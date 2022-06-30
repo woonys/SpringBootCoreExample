1febf926c8934052c19444139318d8e76ddc3eb5
woonys <wodns1324@naver.com>
2022-06-30 22:53:47 +0900

    [test]Memberdomaintest 구현

--- src/main/java/com/woony/core/member/MemberServiceImpl.java
+++ src/main/java/com/woony/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package com.woony.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 구현체(MemoryMemberRepository)에 의존하고 있음! 추상화에도, 구체화에도 의존 -> DIP 위반!
     @Override
     public void join(Member member) {
         memberRepository.save(member);
---
+++ src/test/java/com/woony/core/member/MemberServiceTest.java
@@ -0,0 +1,22 @@
+package com.woony.core.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        // given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        // when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        // then
+        assertThat(member).isEqualTo(findMember);
+    }
+}
