f0e6f9a06c2c0b116ef38aaa8ed93cb3440e3cbc
woonys <wodns1324@naver.com>
2022-07-02 18:12:06 +0900

    [Test]OrderServiceTest

---
+++ src/test/java/com/woony/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package com.woony.core.order;
+
+import com.woony.core.member.Grade;
+import com.woony.core.member.Member;
+import com.woony.core.member.MemberService;
+import com.woony.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+   @Test
+    void createOrder() {
+       Long memberId = 1L;
+       Member member = new Member(memberId, "memberA", Grade.VIP);
+       memberService.join(member);
+
+       Order order = orderService.createOrder(memberId, "itemA", 10000);
+       Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+   }
+}
