0002e16b591d4cd551086642c0ce85282d4837a4
woonys <wodns1324@naver.com>
2022-08-03 21:30:42 +0900

    빈 생명주기 콜백 - 인터페이스 InitializingBean, DisposableBean

--- src/test/java/com/woony/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/woony/core/lifecycle/BeanLifeCycleTest.java
@@ -11,7 +11,7 @@ public class BeanLifeCycleTest {
     @Test
     public void lifeCycleTest() {
         ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
-        NetworkClient client = ac.getBean(NetworkClient.class);
+        NetworkClient client = ac.getBean(NetworkClient.class); // 빈 생성만 하고 호출하진 않음.
         ac.close(); // 얘를 호출해줘야!
     }
 
@@ -19,7 +19,7 @@ public class BeanLifeCycleTest {
     static class LifeCycleConfig {
         @Bean
         public NetworkClient networkClient() {
-            NetworkClient networkClient = new NetworkClient();
+            NetworkClient networkClient = new NetworkClient(); // 여기서 생성자 호출
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
         }
--- src/test/java/com/woony/core/lifecycle/NetworkClient.java
+++ src/test/java/com/woony/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package com.woony.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
-        System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+        System.out.println("생성자 호출, url = " + url); // 처음에는 url 없음
     }
 
     public void setUrl(String url) {
@@ -16,7 +17,7 @@ public class NetworkClient {
 
     // 서비스 시작시 호출
     public void connect() {
-        System.out.println("connect: " + url);
+        System.out.println("connect: " + url); // 여전히 여기 값이 없다.
     }
 
     public void call(String message) {
@@ -26,5 +27,17 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("Close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception { // Property 세팅, 즉 의존관계 주입이 끝나면 얘가 호출!
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
